f14608d5b33e28eaf741a80b7ef3f0a031c25638
meshaibal <me.shaibal@gmail.com>
2017-04-02 09:02:31 +0200

    Commit-4 1. Added persistence dependency in web 2. Loading persistence configuration from web 3. Updated gitignore

--- text-management-web/src/main/java/com/edia/text/management/web/conf/WebMVCInitializer.java
+++ text-management-web/src/main/java/com/edia/text/management/web/conf/WebMVCInitializer.java
@@ -3,6 +3,7 @@ package com.edia.text.management.web.conf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,9 +11,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import com.edia.text.management.persistence.conf.PersistenceLayerConfig;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {WebMVCInitializer.PACKAGE_TEXT_MANAGEMENT_WEB})
+@Import(PersistenceLayerConfig.class)
 public class WebMVCInitializer extends WebMvcConfigurerAdapter{
 
 	public static final String PACKAGE_TEXT_MANAGEMENT_WEB = "com.edia.text.management.web";
