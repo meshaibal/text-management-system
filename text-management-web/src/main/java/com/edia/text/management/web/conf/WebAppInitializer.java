9f55af68f6fa40cfadae73b0f3d0541db585c709
meshaibal <me.shaibal@gmail.com>
2017-04-02 17:05:34 +0200

    Commit-13 1. Added Logger

--- text-management-web/src/main/java/com/edia/text/management/web/conf/WebAppInitializer.java
+++ text-management-web/src/main/java/com/edia/text/management/web/conf/WebAppInitializer.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
@@ -12,9 +13,13 @@ import org.springframework.web.servlet.DispatcherServlet;
 
 public class WebAppInitializer implements WebApplicationInitializer{
 
+	private static final Logger LOG = Logger.getLogger(WebAppInitializer.class);
+	
 	private static final String PACKAGE_WEB_CONFIGIRATION = "com.edia.text.management.web" ;
 	
-	public void onStartup(ServletContext servletContext) throws ServletException {System.out.println("Inside");
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		LOG.info("Initializing Web Layer");
+		
 		WebApplicationContext applicationContext = getContext();
 		
 		servletContext.addListener(new ContextLoaderListener(applicationContext));
--- text-management-web/src/main/java/com/edia/text/management/web/conf/WebLayerConf.java
+++ text-management-web/src/main/java/com/edia/text/management/web/conf/WebLayerConf.java
@@ -15,9 +15,9 @@ import com.edia.text.management.persistence.conf.PersistenceLayerConfig;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {WebMVCInitializer.PACKAGE_TEXT_MANAGEMENT_WEB})
+@ComponentScan(basePackages = {WebLayerConf.PACKAGE_TEXT_MANAGEMENT_WEB})
 @Import(PersistenceLayerConfig.class)
-public class WebMVCInitializer extends WebMvcConfigurerAdapter{
+public class WebLayerConf extends WebMvcConfigurerAdapter{
 
 	public static final String PACKAGE_TEXT_MANAGEMENT_WEB = "com.edia.text.management.web";
 	
